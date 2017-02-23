a8cce3358c68686a490294bc7b80bf5405054e9d
Connor Manning <connor@hobu.co>
2017-02-23 11:33:42 -0600

    Use a C and a posix function for get/put_time if possible due to poor compiler support.  Add some basic tests for Time util.

--- arbiter/util/time.cpp
+++ arbiter/util/time.cpp
@@ -2,7 +2,9 @@
 #include <arbiter/util/time.hpp>
 #endif
 
+#include <ctime>
 #include <iomanip>
+#include <iostream>
 #include <mutex>
 #include <sstream>
 
@@ -61,22 +63,41 @@ Time::Time(const std::string& s, const std::string& format)
     static const int64_t utcOffset(utcOffsetSeconds());
 
     auto tm(getTm());
+#ifndef ARBITER_WINDOWS
+    // We'd prefer to use get_time, but it has poor compiler support.
+    if (!strptime(s.c_str(), format.c_str(), &tm))
+    {
+        throw ArbiterError("Failed to parse " + s + " as time: " + format);
+    }
+#else
     std::istringstream ss(s);
     ss >> std::get_time(&tm, format.c_str());
     if (ss.fail())
     {
         throw ArbiterError("Failed to parse " + s + " as time: " + format);
     }
+#endif
     tm.tm_sec -= utcOffset;
     m_time = std::mktime(&tm);
 }
 
 std::string Time::str(const std::string& format) const
 {
-    std::ostringstream ss;
     std::lock_guard<std::mutex> lock(mutex);
+#ifndef ARBITER_WINDOWS
+    // We'd prefer to use put_time, but it has poor compiler support.
+    // We're already locked here for gmtime, so might as well make this static.
+    static std::vector<char> s(256, 0);
+
+    const std::size_t size =
+        strftime(s.data(), s.size(), format.c_str(), std::gmtime(&m_time));
+
+    return std::string(s.data(), s.data() + size);
+#else
+    std::ostringstream ss;
     ss << std::put_time(std::gmtime(&m_time), format.c_str());
     return ss.str();
+#endif
 }
 
 int64_t Time::operator-(const Time& other) const
--- test/unit.cpp
+++ test/unit.cpp
@@ -1,6 +1,9 @@
+#include <arbiter/util/time.hpp>
 #include <arbiter/arbiter.hpp>
 
 #include "gtest/gtest.h"
+
+using namespace arbiter;
 
 TEST(Env, HomeDir)
 {
@@ -13,10 +16,26 @@ TEST(Env, HomeDir)
 
 TEST(Arbiter, HttpDerivation)
 {
-    arbiter::Arbiter a;
+    Arbiter a;
     EXPECT_TRUE(a.isHttpDerived("http://arbitercpp.com"));
     EXPECT_FALSE(a.isHttpDerived("~/data"));
     EXPECT_FALSE(a.isHttpDerived("."));
+}
+
+TEST(Arbiter, Time)
+{
+    Time a;
+    Time b(a.str());
+
+    EXPECT_EQ(a.str(), b.str());
+    EXPECT_EQ(a - b, 0);
+
+    Time x("2016-03-18T03:14:42Z");
+    Time y("2016-03-18T04:24:54Z");
+    const int64_t delta(1 * 60 * 60 + 10 * 60 + 12);
+
+    EXPECT_EQ(y - x, delta);
+    EXPECT_EQ(x - y, -delta);
 }
 
 int main(int argc, char** argv)
