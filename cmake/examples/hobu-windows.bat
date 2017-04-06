6a4587db11e484546e380a13715e2afcfb4e0e8a
Howard Butler <howard@hobu.co>
2017-04-06 13:59:06 -0500

    add a hobu example invocation for windows

---
+++ cmake/examples/hobu-windows.bat
@@ -0,0 +1,26 @@
+@echo on
+
+:: This configure script is designed for the default Windows world, 
+
+:: This configure script expects to be run from the PDAL root directory.
+
+:: Pick your CMake GENERATOR.  (NMake will pick up architecture (x32, x64) from your environment)
+set GENERATOR="NMake Makefiles"
+REM set GENERATOR="Visual Studio 14 Win64"
+rem set GENERATOR="Visual Studio 10"
+
+:: Pick your build type
+set BUILD_TYPE=Release
+REM set BUILD_TYPE=Debug
+
+
+if EXIST CMakeCache.txt del CMakeCache.txt
+
+cmake -G %GENERATOR% ^
+    -DCMAKE_INSTALL_PREFIX=C:\pdalbin ^
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON ^
+	-DBUILD_SHARED_LIBS=ON ^
+    -Dgtest_force_shared_crt=ON ^
+    .
+	
