67761eb4ddfeac89e1a0d73aab21f2f081e1f484
Connor Manning <connor@hobu.co>
2022-02-15 13:28:48 -0600

    Make sure all AZURE_ env vars are also aliased to AZ_.

--- arbiter/drivers/az.cpp
+++ arbiter/drivers/az.cpp
@@ -255,6 +255,14 @@ std::string AZ::Config::extractService(
     {
         return *p;
     }
+    else if (auto p = env("AZ_SERVICE"))
+    {
+        return *p;
+    }
+    else if (auto p = env("AZ_DEFAULT_SERVICE"))
+    {
+        return *p;
+    }
 
     if (!c.is_null() && c.value("verbose", false))
     {
@@ -275,6 +283,10 @@ std::string AZ::Config::extractEndpoint(
         return c.at("endpoint").get<std::string>();
     }
     else if (auto p = env("AZURE_ENDPOINT"))
+    {
+        return *p;
+    }
+    else if (auto p = env("AZ_ENDPOINT"))
     {
         return *p;
     }
